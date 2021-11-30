af9b5a349cdb5736cbc7b0115fb62fa4278e05e2
AnujPundir29 <pundiranuj28@gmail.com>
2021-12-01 03:00:11 +0530

    sync between players paddle

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ server.listen(3000, () => {
     console.log('listening on port 3000')
 });
 
+let countPlayer = 0;
+
 io.on('connection', (socket) => {
     console.log('a user got connected '+socket.id);
+
+    socket.on('ready',()=>{
+        console.log(`Player ${countPlayer+1} as ${socket.id} is ready!!`);
+
+        countPlayer++;
+        
+        if(countPlayer === 2)
+        {
+            // broadcast start game to all clients
+            io.emit('startGame', socket.id);
+        }
+    });
+
+    socket.on('paddleMove',(paddleData)=>{
+        // broadcast to all client except the sender    
+        socket.broadcast.emit('paddleMove', paddleData);
+    })
+    
 })
