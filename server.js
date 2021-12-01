dbd75a22c6332afc81b23bf5fd9bb2fcbb10ade0
AnujPundir29 <pundiranuj28@gmail.com>
2021-12-02 00:58:22 +0530

    cleaning our code

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const api = require('./api');
 const http = require('http');
 const io = require('socket.io');
-const httpServer = http.createServer(api);
-const sockets = require('./sockets');
-const socketServer = io(httpServer, {
-    cors: {
-        origin: '*',
-    }
-});
 
-socketServer.listen(3000, () => {
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer);
+
+const sockets = require('./sockets');
+
+httpServer.listen(3000, () => {
     console.log('listening on port 3000')
 });
 
-sockets.listen(io);
+sockets.listen(socketServer);
