dab2220a3897e9fd5543e2a7c6e5c77a10a20e58
AnujPundir29 <pundiranuj28@gmail.com>
2021-12-02 01:34:39 +0530

    created rooms using namespace

--- sockets.js
+++ sockets.js
@@ -3,30 +3,42 @@ let countPlayer = 0;
 function listen(io) {
     const pongNamespace = io.of('/pong');  // only listen to pong namespace
     pongNamespace.on('connection', (socket) => {
+        let room;
         console.log('a user got connected ' + socket.id);
 
         socket.on('ready', () => {
-            console.log(`Player ${countPlayer+1} as ${socket.id} is ready!!`);
+            room = `room ${Math.floor(countPlayer/2)}`;
+
+            socket.join(room);  // creating a room and joining it
+            
+            console.log(`Player ${countPlayer+1} as ${socket.id} is ready!! in room ${room}`);
 
             countPlayer++;
 
             if (countPlayer % 2 === 0) {
                 // broadcast start game to all clients
-                pongNamespace.emit('startGame', socket.id);
+                // pongNamespace.emit('startGame', socket.id);
+
+                //broadcast to a specific room
+                pongNamespace.in(room).emit('startGame', socket.id);
             }
         });
 
         socket.on('paddleMove', (paddleData) => {
             // broadcast to all client except the sender    
-            socket.broadcast.emit('paddleMove', paddleData);
+            // socket.broadcast.emit('paddleMove', paddleData);
+
+            //broadcast to a specific room
+            socket.to(room).emit('paddleMove', paddleData);
         });
 
         socket.on('ballMove', (ballData) => {
-            socket.broadcast.emit('ballMove', ballData);
+            socket.to(room).emit('ballMove', ballData);
         })
 
         socket.on('disconnect', (reason) => {
             console.log(`Client ${socket.id} disconnected because of ${reason}`);
+            socket.leave(room)
         })
     })
 }
