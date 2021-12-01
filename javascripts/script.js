16e6711eac0b9144e8f928cf2340d565aa12d36a
AnujPundir29 <pundiranuj28@gmail.com>
2021-12-02 00:29:47 +0530

    added events for ballMove and paddleMove

--- javascripts/script.js
+++ javascripts/script.js
@@ -90,6 +90,11 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  });
 }
 
 // Adjust Ball Movement
@@ -100,6 +105,11 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score,
+  });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -158,9 +168,11 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
@@ -211,4 +223,12 @@ socket.on('paddleMove', (paddleData) => {
   // toggle 1 to 0 and vice versa 
   const opponentPaddleIndex = 1 - paddleIndex;
   paddleX[opponentPaddleIndex] = paddleData.xPosition;
-})
+});
+
+socket.on('ballMove', (ballData) => {
+  ({
+    ballX,
+    ballY,
+    score
+  } = ballData)
+});
--- server.js
+++ server.js
@@ -13,23 +13,29 @@ server.listen(3000, () => {
 let countPlayer = 0;
 
 io.on('connection', (socket) => {
-    console.log('a user got connected '+socket.id);
+    console.log('a user got connected ' + socket.id);
 
-    socket.on('ready',()=>{
+    socket.on('ready', () => {
         console.log(`Player ${countPlayer+1} as ${socket.id} is ready!!`);
 
         countPlayer++;
-        
-        if(countPlayer === 2)
-        {
+
+        if (countPlayer % 2 === 0) {
             // broadcast start game to all clients
             io.emit('startGame', socket.id);
         }
     });
 
-    socket.on('paddleMove',(paddleData)=>{
+    socket.on('paddleMove', (paddleData) => {
         // broadcast to all client except the sender    
         socket.broadcast.emit('paddleMove', paddleData);
+    });
+
+    socket.on('ballMove', (ballData) => {
+        socket.broadcast.emit('ballMove', ballData);
     })
-    
+
+    socket.on('disconnect', (reason) => {
+        console.log(`Client ${socket.id} disconnected because of ${reason}`);
+    })
 })
